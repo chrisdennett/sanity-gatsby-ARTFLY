94a2918955904db413d1a0fc6d83ec6e6fcd7476
chrisdennett <chrisdennett@gmail.com>
2019-10-03 17:35:09 +0100

    Removed related projects.

--- web/src/templates/project.js
+++ web/src/templates/project.js
@@ -11,13 +11,6 @@ export const query = graphql`
     project: sanityProject(id: { eq: $id }) {
       id
       publishedAt
-      relatedProjects {
-        title
-        _id
-        slug {
-          current
-        }
-      }
       mainImage {
         crop {
           _key
@@ -45,34 +38,6 @@ export const query = graphql`
         current
       }
       _rawBody
-      members {
-        _key
-        person {
-          image {
-            crop {
-              _key
-              _type
-              top
-              bottom
-              left
-              right
-            }
-            hotspot {
-              _key
-              _type
-              x
-              y
-              height
-              width
-            }
-            asset {
-              _id
-            }
-          }
-          name
-        }
-        roles
-      }
     }
   }
 `;
