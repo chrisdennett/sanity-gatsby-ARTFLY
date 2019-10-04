cfe6a0d8d25e1d9fef75050c6e9f430536a3dea3
chrisdennett <chrisdennett@gmail.com>
2019-10-04 12:25:42 +0100

    Just tweaking dashboard layout.

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -12,16 +12,16 @@ export default {
                 "NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.",
               sites: [
                 {
-                  buildHookId: "5d95e73828b476bbae90dd96",
-                  title: "Sanity Studio",
-                  name: "sanity-gatsby-ARTFLY-studio",
-                  apiId: "400e40b7-5dc3-4bf5-8833-09de5b684156"
-                },
-                {
                   buildHookId: "5d95e7388a1c180fe5ef677b",
                   title: "Portfolio Website",
                   name: "sanity-gatsby-ARTFLY",
                   apiId: "e5d7d1c4-9778-4a15-b8bd-ca663e3693bd"
+                },
+                {
+                  buildHookId: "5d95e73828b476bbae90dd96",
+                  title: "Sanity Studio",
+                  name: "sanity-gatsby-ARTFLY-studio",
+                  apiId: "400e40b7-5dc3-4bf5-8833-09de5b684156"
                 }
               ]
             }
