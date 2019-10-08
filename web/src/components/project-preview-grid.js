b4cd08a3d7b221273e260739390650f9c7930e6a
chris <chrisdennett@gmail.com>
2019-10-08 21:11:06 +0100

    Refactored project preview grid to use styled components.

--- web/src/components/project-preview-grid.js
+++ web/src/components/project-preview-grid.js
@@ -1,34 +1,73 @@
-import {Link} from 'gatsby'
-import React from 'react'
-import ProjectPreview from './project-preview'
+import { Link } from "gatsby";
+import styled from "styled-components";
+import React from "react";
+import ProjectPreview from "./project-preview";
 
-import styles from './project-preview-grid.module.css'
-
-function ProjectPreviewGrid (props) {
+const ProjectPreviewGrid = ({ title, nodes, browseMoreHref }) => {
   return (
-    <div className={styles.root}>
-      {props.title && <h2 className={styles.headline}>{props.title}</h2>}
-      <ul className={styles.grid}>
-        {props.nodes &&
-          props.nodes.map(node => (
+    <StyledGrid>
+      {title && <HeaderStyled>{title}</HeaderStyled>}
+      <GridUlStyled>
+        {nodes &&
+          nodes.map(node => (
             <li key={node.id}>
               <ProjectPreview {...node} />
             </li>
           ))}
-      </ul>
-      {props.browseMoreHref && (
-        <div className={styles.browseMoreNav}>
-          <Link to={props.browseMoreHref}>Browse more</Link>
-        </div>
+      </GridUlStyled>
+      {browseMoreHref && (
+        <BrowserMoreNavStyled>
+          <Link to={browseMoreHref}>Browse more</Link>
+        </BrowserMoreNavStyled>
       )}
-    </div>
-  )
-}
+    </StyledGrid>
+  );
+};
 
-ProjectPreviewGrid.defaultProps = {
-  title: '',
-  nodes: [],
-  browseMoreHref: ''
-}
+export default ProjectPreviewGrid;
 
-export default ProjectPreviewGrid
+const StyledGrid = styled.div`
+  margin: 2em 0 4em;
+`;
+
+const HeaderStyled = styled.h2`
+  font-size: 10px;
+  line-height: 12px;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+  margin: 2rem 0;
+`;
+
+const GridUlStyled = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 2em;
+
+  @media (min-width: 450px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 675px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
+
+const BrowserMoreNavStyled = styled.div`
+  margin-top: 1rem;
+  text-align: right;
+
+  a {
+    display: inline-block;
+    padding: 0.5rem 0;
+    color: inherit;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: red;
+  }
+`;
