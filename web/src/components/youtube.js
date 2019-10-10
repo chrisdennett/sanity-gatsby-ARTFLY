b57fc2d786cc1675c214cffbd23084e65a3d6087
chrisdennett <chrisdennett@gmail.com>
2019-10-10 10:48:52 +0100

    Made youtube embed responsive.

--- web/src/components/youtube.js
+++ web/src/components/youtube.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import getYouTubeId from "get-youtube-id";
 import YouTube from "react-youtube";
 
@@ -8,5 +9,24 @@ export default ({ node }) => {
   }
 
   const id = getYouTubeId(node.url);
-  return <YouTube videoId={id} />;
+  return (
+    <IFrameWrapperStyled>
+      <YouTube videoId={id} />;
+    </IFrameWrapperStyled>
+  );
 };
+
+const IFrameWrapperStyled = styled.div`
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  padding-top: 25px;
+  height: 0;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
