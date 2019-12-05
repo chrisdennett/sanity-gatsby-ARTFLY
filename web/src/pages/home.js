dae6ba5094c3da00a8bb5517812fa35a0b4fe408
chrisdennett <chrisdennett@gmail.com>
2019-12-05 10:47:21 +0000

    Separated projects on home page into featured, other and drafts.

--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -5,9 +5,39 @@ import ProjectPreviewGrid from "../components/project-preview-grid";
 import ProjectTags from "../components/project-tags";
 
 const Home = ({ projectNodes, tagNodes }) => {
+  const featuredProjects = projectNodes.filter(
+    proj => proj.projectTypes === "project" && proj.isFeatured === true
+  );
+
+  const otherProjects = projectNodes.filter(
+    proj => proj.projectTypes === "project" && proj.isFeatured !== true
+  );
+
+  const draftProjects = projectNodes.filter(proj => proj.projectTypes === "draft");
+
   return (
     <Wrapper>
-      {projectNodes && <ProjectPreviewGrid nodes={projectNodes} />}
+      <h2>Featured Projects:</h2>
+      {projectNodes && (
+        <>
+          <ProjectPreviewGrid nodes={featuredProjects} />
+        </>
+      )}
+
+      {otherProjects.length > 0 && (
+        <>
+          <h3>Other Projects:</h3>
+          <ProjectPreviewGrid nodes={otherProjects} />
+        </>
+      )}
+
+      {draftProjects && (
+        <>
+          <h3>DRAFT PROJECTS</h3>
+          <p>These haven't be written up yet - basically this is our todo list made public!</p>
+          <ProjectPreviewGrid nodes={draftProjects} />
+        </>
+      )}
 
       {tagNodes && (
         <TAGS_HOLDER>
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -6,6 +6,43 @@ import SEO from "../components/seo";
 import Layout from "../containers/layout";
 import Home from "./home";
 
+const IndexPage = props => {
+  const { data, errors } = props;
+
+  // show error page if error thrown
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    );
+  }
+
+  // get all pages with slugs
+  const site = (data || {}).site;
+  const projectNodes = (data || {}).projects
+    ? mapEdgesToNodes(data.projects).filter(filterOutDocsWithoutSlugs)
+    : [];
+  const tagNodes = (data || {}).tags
+    ? mapEdgesToNodes(data.tags).filter(filterOutDocsWithoutSlugs)
+    : [];
+
+  if (!site) {
+    throw new Error(
+      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+    );
+  }
+
+  return (
+    <Layout>
+      <SEO title={site.title} description={site.description} keywords={site.keywords} />
+      <Home projectNodes={projectNodes} tagNodes={tagNodes} />
+    </Layout>
+  );
+};
+
+export default IndexPage;
+
 export const query = graphql`
   query IndexPageQuery {
     site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
@@ -58,6 +95,8 @@ export const query = graphql`
             alt
           }
           title
+          projectTypes
+          isFeatured
           _rawExcerpt
           slug {
             current
@@ -67,42 +106,3 @@ export const query = graphql`
     }
   }
 `;
-
-const IndexPage = props => {
-  const { data, errors } = props;
-
-  if (errors) {
-    return (
-      <Layout>
-        <GraphQLErrorList errors={errors} />
-      </Layout>
-    );
-  }
-
-  // .filter(filterOutDocsPublishedInTheFuture)
-
-  const site = (data || {}).site;
-
-  const projectNodes = (data || {}).projects
-    ? mapEdgesToNodes(data.projects).filter(filterOutDocsWithoutSlugs)
-    : [];
-
-  const tagNodes = (data || {}).tags
-    ? mapEdgesToNodes(data.tags).filter(filterOutDocsWithoutSlugs)
-    : [];
-
-  if (!site) {
-    throw new Error(
-      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    );
-  }
-
-  return (
-    <Layout>
-      <SEO title={site.title} description={site.description} keywords={site.keywords} />
-      <Home projectNodes={projectNodes} tagNodes={tagNodes} />
-    </Layout>
-  );
-};
-
-export default IndexPage;
