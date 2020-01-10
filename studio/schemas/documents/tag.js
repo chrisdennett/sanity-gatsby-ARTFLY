11594376a50b3dcb1b75ea65f45c2192ce723f9d
chrisdennett <chrisdennett@gmail.com>
2020-01-10 14:56:17 +0000

    replaced problem entry with a string

--- studio/schemas/documents/tag.js
+++ studio/schemas/documents/tag.js
@@ -40,16 +40,7 @@ export default {
     {
       name: "contactDetails",
       title: "Contact Details",
-      type: "document",
-      options: { collapsible: true, collapsed: false },
-      fields: [
-        { name: "email", type: "string", title: "Email" },
-        { name: "twitter", type: "string", title: "Twitter" },
-        { name: "instagram", type: "string", title: "Instagram" },
-        { name: "facebook", type: "string", title: "Facebook" },
-        { name: "linkedIn", type: "string", title: "LinkedIn" },
-        { name: "youTube", type: "string", title: "YouTube" }
-      ]
+      type: "string"
     },
     {
       name: "bio",
